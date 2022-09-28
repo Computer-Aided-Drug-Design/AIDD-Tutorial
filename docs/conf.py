432c5f4ccc3863dcf12ad5c94d698234261f20db
Abdusemi Abduweli <abdusemiabduweli@gmail.com>
2022-09-28 22:05:51 +0800

    update

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = '人工智能药物设计教程'
+author = '2022, Abdusemi Abduweli'
+
+release = ''
+version = ''
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = "furo"
+
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+欢迎光临！
+========================================================
+
+此项目（包括此网站）由社区管理。`Github地址点这里`_。
+
+.. _Github地址点这里: https://github.com/abdusemiabduweli/AIDD-Tutorial
+
+此项目创始人：Abdusemi
+
+内容：
+------------------------------
+
+
+.. toctree::
+    DeepChem
+    The Basic Tools of the Deep Life Sciences
